a7748588d1f39902aa0e7148c6eeff23e9e2454c
Arsdev998 <arsdev998@gmail.com>
2024-08-21 17:09:23 +0700

    searh params

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,25 @@ import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import CartPage from "./pages/CartPage";
 import HomePage from "./pages/HomePage";
-import { Routes, Route ,useLocation} from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import NotFoundPage from "./pages/NotFoundPage";
 import LoginPage from "./pages/LoginPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import ProductManagementPage from "./pages/admin/ProductManagementPage";
 function App() {
-  const location = useLocation()
-  console.log(location.pathname);
-  
+  const location = useLocation();
   return (
     <>
-      <Header />
+      {!location.pathname.startsWith("/admin") ? <Header /> : null}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/product/:id" element={<ProductDetailPage />} />
-        <Route path="/admin/products" element={<ProductManagementPage/>} />
-        <Route path="*" element={<NotFoundPage/>} />
+        <Route path="/admin/products" element={<ProductManagementPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
-      <Footer />
+      {!location.pathname.startsWith("/admin") ? <Footer /> : null}
     </>
   );
 }
--- src/pages/admin/ProductManagementPage.jsx
+++ src/pages/admin/ProductManagementPage.jsx
@@ -1,11 +1,82 @@
 import AdminLayout from "@/components/layout/AdminLayout";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+} from "@/components/ui/pagination";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { axiosIstance } from "@/lib/axios";
+import { ChevronLeft, ChevronRight, Ellipsis } from "lucide-react";
+import { useEffect, useState } from "react";
 import { IoAdd } from "react-icons/io5";
+import { useParams, useSearchParams } from "react-router-dom";
 
 const ProductManagementPage = () => {
+  const [productsData, setProductsData] = useState([]);
+  const [lastPage, setLastPage] = useState(1);
+  const [hashNextPage, setHashNextPage] = useState(true);
+  const [searchParam, setSearchparams] = useSearchParams();
+  const [productName, setProductName] = useState("");
+
+  const nextPage = () => {
+    searchParam.set("page", Number(searchParam.get("page")) + 1);
+    setSearchparams(searchParam);
+  };
+  const prevPage = () => {
+    searchParam.set("page", Number(searchParam.get("page")) - 1);
+    setSearchparams(searchParam);
+  };
+  const fetchProduct = async () => {
+    try {
+      const response = await axiosIstance.get("/products?", {
+        params: {
+          _per_page: 5,
+          _page: searchParam.get("page"),
+          title: searchParam.get("search"),
+        },
+      });
+      console.log(response.data);
+      setProductsData(response.data);
+      setHashNextPage(Boolean(response.data.next));
+      setLastPage(Number(response.data.last));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const searhProduct = () => {
+    if(productName){
+      searchParam.set("search", productName);
+      setSearchparams(searchParam);
+    }else{
+      searchParam.delete("search");
+    }
+  };
+  useEffect(() => {
+    if (searchParam.get("page")) {
+      fetchProduct();
+    }
+  }, [searchParam.get("page"), searchParam.get("search")],);
+
+  useEffect(() => {
+    if (!searchParam.get("page")) {
+      searchParam.set("page", 1);
+      setSearchparams(searchParam);
+    }
+  }, []);
+
   return (
     <>
-      <div>ProductManagementPage</div>
       <AdminLayout
         title="Products Management"
         description="Managing our product"
@@ -15,7 +86,75 @@ const ProductManagementPage = () => {
             Add Product
           </Button>
         }
-      ></AdminLayout> 
+      >
+        <div className="">
+          <Label>Search product name</Label>
+          <div className="flex gap-4 mb-5">
+            <Input
+              placeholder="Search Products"
+              value={productName}
+              onChange={(e) => setProductName(e.target.value)}
+              className="max-w-[400px]"
+            />
+            <Button onClick={searhProduct}>Search</Button>
+          </div>
+        </div>
+        <Table className="p-4 border rounded-md">
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>Peoduct Name</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead>Stock</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {productsData.data?.map((product) => {
+              return (
+                <TableRow key={product.id}>
+                  <TableCell>{product.id}</TableCell>
+                  <TableCell>{product.title}</TableCell>
+                  <TableCell>
+                    Rp.{product.price?.toLocaleString("id-ID")}
+                  </TableCell>
+                  <TableCell>{product.stock}</TableCell>
+                  <TableCell>
+                    <Button variant="ghost" size="icon">
+                      <Ellipsis className="w-6 h-6" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+        <Pagination className="mt-8">
+          <PaginationContent>
+            <PaginationItem>
+              <Button
+                variant="ghost"
+                onClick={prevPage}
+                disabled={searchParam.get("page") == 1}
+              >
+                <ChevronLeft className="h-6 w-6 mr-2" /> Prev
+              </Button>
+            </PaginationItem>
+            <PaginationItem className="mx-8 font-semibold">
+              {searchParam.get("page")}
+            </PaginationItem>
+            <PaginationItem>
+              <Button
+                variant="ghost"
+                disabled={!hashNextPage}
+                onClick={nextPage}
+              >
+                <ChevronRight className="h-6 w-6 ml-2" /> Next
+              </Button>
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </AdminLayout>
     </>
   );
 };
